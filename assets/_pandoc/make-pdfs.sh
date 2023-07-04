5efa4979f8a87e0713a93b8197ce7093cd562386
Tyler Farrington <underwaterowlbear@gmail.com>
2023-07-04 16:00:43 -0400

    deleted bullet rules line

--- assets/_pandoc/make-pdfs.sh
+++ assets/_pandoc/make-pdfs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pandoc /pages/cairn-houserules.md --toc --toc-depth=2 --shift-heading-level-by=-1 -s -o cairn-houserules.pdf
 pandoc /pages/bb.md --toc --toc-depth=2 --shift-heading-level-by=-1 -s -o bb.pdf
-pandoc /pages/bullet-rules.md --toc --toc-depth=2 --shift-heading-level-by=-1 -s -o bullet-rules.pdf
